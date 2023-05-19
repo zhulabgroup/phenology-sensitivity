2c6a648c6e5d27a483ddea086e98d03ea401e2ba
Liu22Yi <ly2222ny2sz@gmail.com>
2023-05-19 17:16:43 -0400

    check which combination has the most points

--- scripts/lead_flower_correlation.R
+++ scripts/lead_flower_correlation.R
@@ -52,14 +52,29 @@ npn_quercus <- read_rds("/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/Quercu
 
 npn_simple <- npn_quercus %>%   filter(observed_status_conflict_flag == "-9999") %>% # Removing Status Conflicts
   group_by(individual_id, first_yes_year, pheno_class_id) %>%
-  filter(first_yes_doy == min(first_yes_doy)) %>%
-  ungroup() %>%
+  arrange(first_yes_doy) %>%
+  slice(1)%>%
   mutate(numdays_since_prior_no = na_if(numdays_since_prior_no, "-9999")) %>% # set the -9999 values to NA
   filter(numdays_since_prior_no < 20) %>% # Filtering Data by Prior No
   select(individual_id, first_yes_year, first_yes_doy, species_id, dataset_id,pheno_class_id)
 
+##### check which combination has the most points: 1,7; 2,7 #########
+# wide_table <- npn_simple %>%
+#   pivot_wider(names_from = pheno_class_id, values_from = first_yes_doy)
+# 
+# pairs(wide_table[,c(5:10)])
 
-leafing <- npn_simple %>% filter(pheno_class_id == 1)
+leaf <-npn_simple %>% filter(pheno_class_id %in% c(1, 2, 3))
+
+flower <-npn_simple %>% filter(pheno_class_id %in% c(6, 7, 8))
+
+temp <- inner_join(leaf, flower, by = c("individual_id", "first_yes_year", "species_id", "dataset_id") )
+
+table(temp$pheno_class_id.x,temp$pheno_class_id.y)
+
+#####
+
+leafing <- npn_simple %>% filter(pheno_class_id == 2)
 flowering <- npn_simple %>% filter(pheno_class_id == 7)
 
 joined_data <- inner_join(leafing, flowering, by = c("individual_id", "first_yes_year", "species_id", "dataset_id") )
@@ -71,6 +86,6 @@ ggplot(joined_data) +
   ggtitle("Leafing vs. Flowering") +
   geom_abline(intercept = 0, slope = 1) +
   facet_wrap(~dataset_id)
-  coord_equal() 
 
+  
 

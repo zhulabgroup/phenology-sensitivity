32c392e17c471cd4ed6ecdf20d71b300591b30ec
Liu22Yi <ly2222ny2sz@gmail.com>
2023-05-19 16:25:53 -0400

    compare the result from different datasets

--- scripts/lead_flower_correlation.R
+++ scripts/lead_flower_correlation.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(rnpn)
+
 acer <- c(62,63,319)
 
 npn <- npn_download_individual_phenometrics(
@@ -5,34 +8,33 @@ npn <- npn_download_individual_phenometrics(
   species_ids = acer,
   years = c(as.character(2012:2022)), # years to include
   pheno_class_ids = c(1, 2, 3, 6,7,8),
-  additional_fields = c("ObservedBy_Person_ID","Observed_Status_Conflict_Flag","multiple_firsty","partner_group")
+  additional_fields = c("ObservedBy_Person_ID","Observed_Status_Conflict_Flag","multiple_firsty","partner_group","dataset_id")
 )
 
 
 
-npn_simple <- npn %>%  select(individual_id, first_yes_year, first_yes_doy, pheno_class_id, multiple_firsty, phenophase_description, observedby_person_id,observed_status_conflict_flag,numdays_since_prior_no) %>% 
+npn_simple <- npn %>%  
   filter(observed_status_conflict_flag == "-9999") %>% # Removing Status Conflicts
   group_by(individual_id, first_yes_year, pheno_class_id) %>%
   filter(first_yes_doy == min(first_yes_doy)) %>%
   ungroup() %>%
   mutate(numdays_since_prior_no = na_if(numdays_since_prior_no, "-9999")) %>% # set the -9999 values to NA
-  filter(numdays_since_prior_no < 8)# Filtering Data by Prior No
+  filter(numdays_since_prior_no < 8) %>% # Filtering Data by Prior No
+  select(individual_id, first_yes_year, first_yes_doy, species_id, dataset_id,pheno_class_id)
 
-leafing <- npn_simple %>% filter(pheno_class_id == 1) %>% 
-  select(individual_id, first_yes_year, first_yes_doy)
+leafing <- npn_simple %>% filter(pheno_class_id == 1)
+flowering <- npn_simple %>% filter(pheno_class_id == 6)
 
-flowering <- npn_simple %>% filter(pheno_class_id == 6)%>% 
-  select(individual_id, first_yes_year, first_yes_doy)
+joined_data <- inner_join(leafing, flowering, by = c("individual_id", "first_yes_year", "species_id", "dataset_id") )
 
-joined_data <- inner_join(leafing, flowering, by = c("individual_id", "first_yes_year") )
-
-ggplot(joined_data, aes(x = first_yes_doy.x, y = first_yes_doy.y)) +
-  geom_jitter() +
+ggplot(joined_data) +
+  geom_point(aes(x = first_yes_doy.x, y = first_yes_doy.y, color = as.factor(species_id) ), alpha = 0.5) +
   xlab("Leafing Day") +
   ylab("Flowering Day") +
   ggtitle("Leafing vs. Flowering") +
   geom_abline(intercept = 0, slope = 1) +
-  coord_equal()
+  facet_wrap(~dataset_id)
+  coord_equal() 
 
 ###########
 quercus <- c(705,100,1365,757,1870,987,1690,1484,988,316,297,1485,1190,765,1486,
@@ -43,28 +45,32 @@ npn_quercus <- npn_download_individual_phenometrics(
   species_ids = quercus,
   years = c(as.character(2012:2022)), # years to include
   pheno_class_ids = c(1,6),
-  additional_fields = c("ObservedBy_Person_ID","Observed_Status_Conflict_Flag","multiple_firsty","partner_group")
+  additional_fields = c("ObservedBy_Person_ID","Observed_Status_Conflict_Flag","multiple_firsty","partner_group","dataset_id")
 )
+
+npn_quercus <- read_rds("/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/Quercus.rds")
 
 npn_simple <- npn_quercus %>%   filter(observed_status_conflict_flag == "-9999") %>% # Removing Status Conflicts
   group_by(individual_id, first_yes_year, pheno_class_id) %>%
   filter(first_yes_doy == min(first_yes_doy)) %>%
   ungroup() %>%
   mutate(numdays_since_prior_no = na_if(numdays_since_prior_no, "-9999")) %>% # set the -9999 values to NA
-  filter(numdays_since_prior_no < 8)# Filtering Data by Prior No
+  filter(numdays_since_prior_no < 20) %>% # Filtering Data by Prior No
+  select(individual_id, first_yes_year, first_yes_doy, species_id, dataset_id,pheno_class_id)
 
-leafing <- npn_simple %>% filter(pheno_class_id == 1) %>% 
-  select(individual_id, first_yes_year, first_yes_doy, species_id)
 
-flowering <- npn_simple %>% filter(pheno_class_id == 6)%>% 
-  select(individual_id, first_yes_year, first_yes_doy, species_id)
+leafing <- npn_simple %>% filter(pheno_class_id == 1)
+flowering <- npn_simple %>% filter(pheno_class_id == 7)
 
-joined_data <- inner_join(leafing, flowering, by = c("individual_id", "first_yes_year", "species_id") )
+joined_data <- inner_join(leafing, flowering, by = c("individual_id", "first_yes_year", "species_id", "dataset_id") )
 
 ggplot(joined_data) +
-  geom_point(aes(x = first_yes_doy.x, y = first_yes_doy.y, color = as.factor(species_id) ), alpha = 0.5) +
+  geom_point(aes(x = first_yes_doy.x, y = first_yes_doy.y, color = as.factor(species_id) ), alpha = 0.2) +
   xlab("Leafing Day") +
   ylab("Flowering Day") +
   ggtitle("Leafing vs. Flowering") +
   geom_abline(intercept = 0, slope = 1) +
+  facet_wrap(~dataset_id)
   coord_equal() 
+
+

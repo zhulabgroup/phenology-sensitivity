8c3cdb49c90bef4270b5dd6467fdbb5e7ff7e7ec
Liu22Yi <ly2222ny2sz@gmail.com>
2023-05-24 18:17:02 -0400

    using map to run for each species but failed

--- scripts/npn_plot_all.R
+++ scripts/npn_plot_all.R
@@ -1,6 +1,5 @@
 library(tidyverse)
-library(MASS)
-library(fs)
+source("scripts/function_circular_regression.R")
 # Folder path
 folder_path <- "/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/" 
 
@@ -8,6 +7,8 @@ folder_path <- "/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/"
 rds_files <- list.files(path = folder_path, pattern = "\\.rds$", full.names = TRUE)
 
 # Loop through each RDS file
+fit_model <- vector(mode = "list")
+
 site_gg <- vector(mode = "list")
 for (i in seq_along(rds_files) ) {
   # Read the RDS file
@@ -27,13 +28,30 @@ for (i in seq_along(rds_files) ) {
                  filter(pheno_class_id == 7),
                by = c("individual_id", "first_yes_year", "species_id", "dataset_id"))
   
-  site_gg[[i]] <- ggplot(joined_data) +
-    geom_point(aes(x = first_yes_doy.x, y = first_yes_doy.y, color = as.factor(species_id) ), alpha = 0.2) +
-    geom_smooth(method = "rlm", aes(x = first_yes_doy.x, y = first_yes_doy.y, color = as.factor(species_id)), se = FALSE) +
+  selected_data <- joined_data %>%
+    group_by(species_id) %>%
+    filter(n() > 30) %>% 
+    ungroup() 
+
+  
+  if (nrow(selected_species) == 0) {
+    break
+  }
+
+  
+  fit_data <- selected_data %>% 
+    split(.$species_id) %>%
+    map_df(~mycircular(.$first_yes_doy.x, .$first_yes_doy.y), .id = 'species_id')
+  
+  fit_model[[i]] <- fit_data
+  
+  site_gg[[i]] <- ggplot(fit_data) +
+    geom_point(aes(x = x, y = y, color = "blue")) +
+    geom_point(aes(x = x, y = y_fit, color = "red")) +
     xlab("Leafing Day") +
     ylab("Flowering Day") +
-    ggtitle(basename(rds_files[i])) +
-    geom_abline(intercept = 0, slope = 1)
+    facet_wrap(~species_id, ncol = 3)
+
 }
 
 pdf("/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/taxa_rlm.pdf", width = 8, height = 8 * .618)

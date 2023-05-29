86e67a1fbce65d92c70f3a505155ea6122980e99
Liu22Yi <ly2222ny2sz@gmail.com>
2023-05-29 18:32:18 -0400

    download climate data

--- scripts/npn_download.R
+++ scripts/npn_download.R
@@ -4,7 +4,7 @@ library(rnpn)
 # download data
 v_taxa <- c("Quercus", "Cupressaceae", "Ambrosia", "Morus", "Pinaceae", "Ulmus", "Fraxinus", "Betula", "Poaceae", "Acer", "Populus")
 
-path_npn <- "/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/"
+path_npn <- "/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/climate/"
 # npn_phenophases <- rnpn::npn_phenophases()
 npn_species <- rnpn::npn_species()
 
@@ -19,7 +19,8 @@ for (taxaoi_short in v_taxa) {
   species_ids = spid,
   years = c(2000:2023), # years to include
   pheno_class_ids = c(1, 2, 3, 6,7,8),
-  additional_fields = c("ObservedBy_Person_ID","Observed_Status_Conflict_Flag","multiple_firsty","partner_group","dataset_id")
+  additional_fields = c("ObservedBy_Person_ID","Observed_Status_Conflict_Flag","multiple_firsty","partner_group","dataset_id"),
+  climate_data = TRUE
 )
   write_rds(npn, str_c(path_npn, taxaoi_short, ".rds"))
 }

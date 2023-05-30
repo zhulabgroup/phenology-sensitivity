d8e21fb432f2fe7db9ed7f071fc97836b24453c9
Yi22Liu <ly2222ny2sz@gmail.com>
2023-05-30 19:39:25 -0400

    further explore circular statistics

--- scripts/circular_regression.R
+++ scripts/circular_regression.R
@@ -95,3 +95,19 @@ ggplot(df_circ_fit) +
 plot(df_circ_fit$x_off, df_circ_fit$y_off, col = "blue", pch = 16, main = "Circular Regression")
 # Adding x against fitted_circular_y
 points(df_circ_fit$x_off, df_circ_fit$y_fit_off , col = "red", pch = 16)
+
+
+# why the model can't find the way I build the data
+# Generate a data set of dependent circular variables.
+x <- circular(runif(50, 0, 2*pi))
+y <- atan2(0.15*cos(x) + 0.25*sin(x), 0.35*sin(x)) 
+# + 
+#   rvonmises(n=50, mu=circular(0), kappa=5)
+
+# Fit a circular-circular regression model.
+circ.lm <- lm.circular(y, x, order=1)
+# Obtain a crude plot of the data and fitted regression line.
+plot.default(x, y)
+circ.lm$fitted[circ.lm$fitted>pi] <- circ.lm$fitted[circ.lm$fitted>pi] - 2*pi 
+
+points.default(x[order(x)], circ.lm$fitted[order(x)], type='l')

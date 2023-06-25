b2f3c5e1289942417022ab230217cda46e03e5fa
Liu22Yi <ly2222ny2sz@gmail.com>
2023-06-25 14:57:29 -0400

    finish plotting nab and npn together for acer

--- scripts/generate_nab_npn_data.R
+++ scripts/generate_nab_npn_data.R
@@ -55,3 +55,23 @@ for (i in 1:nrow(stations_sf)) {
 
 names(points_within_buffer) <- station_info$id
 
+library(lubridate)
+
+site_gg <- vector(mode = "list")
+
+for (i in seq_along(points_within_buffer) ) {
+
+  id <- names(points_within_buffer[i])
+  
+  sample_nab <- nab_acer %>% filter(stationid==id)
+  
+  site_gg[[i]] <- ggplot() + 
+    geom_tile(data = sample_nab, aes(x = yday(date), y = year(date), fill = ifelse(count == 0, 0, log(count)))) + # loged
+    scale_fill_gradient(low = "white", high = "brown") +
+    labs(x = "Day of year", y = "Year") +
+    geom_point(data = test, aes(x = first_yes_doy, y = first_yes_year))
+}
+
+pdf("/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/leaf_pollen.pdf", width = 8, height = 8 * .618)
+print(site_gg)
+dev.off()

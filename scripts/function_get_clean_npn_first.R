4ff1abe0f2400988180011a215de36dbe33712b0
Liu22Yi <ly2222ny2sz@gmail.com>
2023-07-13 16:03:15 -0400

    fix the oak zero 365 problem and make the pre process an individual function

---
+++ scripts/function_get_clean_npn_first.R
@@ -0,0 +1,43 @@
+# data pre process for npn first day data
+
+get_clean_npn_first <- function(taxadata){
+  
+data_qc <- taxadata %>%   filter(observed_status_conflict_flag == "-9999") %>% # Removing Status Conflicts
+  group_by(individual_id, first_yes_year, pheno_class_id) %>%
+  arrange(first_yes_doy) %>%
+  slice(1)%>%
+  mutate(numdays_since_prior_no = na_if(numdays_since_prior_no, "-9999")) %>% # set the -9999 values to NA
+  filter(numdays_since_prior_no < 20) %>% # Filtering Data by Prior No
+  dplyr::select(individual_id, first_yes_year, first_yes_doy, species_id, dataset_id, pheno_class_id)
+
+# visial check the normal distribution
+# data_qc %>%
+#    group_by(pheno_class_id, species_id) %>%
+#    filter(n() > 30) %>%
+#    ungroup() %>%
+#    ggplot() +
+#    geom_histogram(aes(x = first_yes_doy), bins = 30) +
+#    facet_grid(species_id ~ pheno_class_id)
+
+data_qc_outlier <- data_qc %>%
+  group_by(pheno_class_id, species_id) %>%
+  filter(n() > 30) %>%
+  summarise(aver = mean(first_yes_doy),
+            std = sd(first_yes_doy),
+            lower = aver - 1.645 * std,
+            upper = aver + 1.645 * std) %>%
+  inner_join(data_qc, by = c("pheno_class_id", "species_id")) %>%
+  mutate(first_yes_year = if_else(first_yes_doy > upper, first_yes_year + 1, as.double(first_yes_year)),
+         first_yes_doy = if_else(first_yes_doy > upper, first_yes_doy - 365, as.double(first_yes_doy) )) %>% 
+  filter(first_yes_doy > lower) %>%
+  ungroup() %>% 
+  dplyr::select(-aver,-std,-lower,-upper)
+
+joined_data <- data_qc_outlier %>%
+  filter(pheno_class_id == 1) %>%
+  inner_join(data_qc_outlier %>%
+               filter(pheno_class_id == 7),
+             by = c("individual_id", "first_yes_year", "species_id", "dataset_id")) %>% 
+  group_by(species_id) %>% 
+  filter(n()>30) 
+}
--- scripts/plot_rlm_combined_8genuses.R
+++ scripts/plot_rlm_combined_8genuses.R
@@ -1,64 +1,34 @@
 # combined figure for rlm for npn data:
-
+source("scripts/function_get_clean_npn_first.R")
 library(MASS)
 
 # Folder path
 folder_path <- "/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/" 
 
 # Get list of RDS files in the folder
-rds_files <- list.files(path = folder_path, pattern = "\\.rds$", full.names = TRUE)[c(1, 3, 5, 7, 8, 9, 10, 11)] 
+rds_files <- list.files(path = folder_path, pattern = "\\.rds$", full.names = TRUE)[c(1, 3, 5, 7, 9, 10)] 
 
 
 # Loop through each RDS file
 site_gg <- vector(mode = "list")
 for (i in seq_along(rds_files) ) {
   # Read the RDS file
-  data <- read_rds(rds_files[i])
+  taxadata <- read_rds(rds_files[i]) %>% 
+    get_clean_npn_first()
   
-  data_qc <- data %>%   filter(observed_status_conflict_flag == "-9999") %>% # Removing Status Conflicts
-    group_by(individual_id, first_yes_year, pheno_class_id) %>%
-    arrange(first_yes_doy) %>%
-    slice(1)%>%
-    mutate(numdays_since_prior_no = na_if(numdays_since_prior_no, "-9999")) %>% # set the -9999 values to NA
-    filter(numdays_since_prior_no < 20) %>% # Filtering Data by Prior No
-    dplyr::select(individual_id, first_yes_year, first_yes_doy, species_id, dataset_id, pheno_class_id)
+  pattern <- "//(.*?)\\.rds"
   
- # visial check the normal distribution
- # data_qc %>%
- #    group_by(pheno_class_id, species_id) %>%
- #    filter(n() > 30) %>%
- #    ungroup() %>%
- #    ggplot() +
- #    geom_histogram(aes(x = first_yes_doy), bins = 30) +
- #    facet_grid(species_id ~ pheno_class_id)
-    
-  data_qc_outlier <- data_qc %>%
-    group_by(pheno_class_id, species_id) %>%
-    filter(n() > 30) %>%
-    summarise(aver = mean(first_yes_doy),
-              std = sd(first_yes_doy),
-              lower = aver - 1.645 * std,
-              upper = aver + 1.645 * std) %>%
-    inner_join(data_qc, by = c("pheno_class_id", "species_id")) %>%
-    filter(first_yes_doy > lower & first_yes_doy < upper) %>%
-    ungroup()
-  
-  joined_data <- data_qc_outlier %>%
-    filter(pheno_class_id == 1) %>%
-    inner_join(data_qc_outlier %>%
-                 filter(pheno_class_id == 7),
-               by = c("individual_id", "first_yes_year", "species_id", "dataset_id"))
-  
-  site_gg[[i]] <- ggplot(joined_data) +
+ 
+  site_gg[[i]] <- ggplot(taxadata) +
     geom_point(aes(x = first_yes_doy.x, y = first_yes_doy.y, color = as.factor(species_id) ), alpha = 0.1) +
     geom_smooth(method = "rlm", aes(x = first_yes_doy.x, y = first_yes_doy.y, color = as.factor(species_id)), se = FALSE) +
     geom_smooth(method = "rlm", aes(x = first_yes_doy.x, y = first_yes_doy.y), color = "#000000",linewidth = 2, se = FALSE) +
     xlab("Leafing Day") +
     ylab("Flowering Day") +
-    ggtitle(as.character(data$genus[1]) ) +
+    ggtitle(stringr::str_extract(rds_files[i], "(?<=//)(.*?)(?=\\.rds)")) +
     geom_abline(intercept = 0, slope = 1)+
     guides(color = FALSE)
   
 }
 
-combined_plot <- patchwork::wrap_plots(site_gg, nrow = 2, ncol = 4)
+combined_plot <- patchwork::wrap_plots(site_gg, nrow = 2, ncol = 3)

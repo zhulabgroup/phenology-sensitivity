fdce358d69907bd46d60246f946b475bbcdbd022
Liu22Yi <ly2222ny2sz@gmail.com>
2023-07-21 17:23:21 -0400

    finish drawing all figures needed for diagram of method

--- scripts/plot_diagram_for_method.R
+++ scripts/plot_diagram_for_method.R
@@ -31,14 +31,41 @@ create_species_plot <- function(target_id) {
 }
 
 # Species IDs to loop through
-species_ids <- c(102, 100, 1159)
+species_ids <- c(1159,102, 100)
 
 # Create a list of plots for each species
 species_plots <- map(species_ids, create_species_plot)
 
 # Arrange the plots vertically using patchwork
-arranged_plots <- reduce(species_plots, `+`)
+arranged_plots <- wrap_plots(species_plots, nrow = length(species_plots))
 arranged_plots
 
+arranged_plots <- plot_grid(plotlist = species_plots, ncol = 1)
 
+# create a figure for log all
 
+with_lag <- sample_station %>% 
+  left_join(lag, "species_id") %>% 
+  mutate(lagged = doy+avelag) %>% 
+  filter(species_id %in% species_ids) 
+
+with_lag %>%
+  ggplot() +
+  geom_tile(aes(x = doy, y = reorder(individual_id, species_id), fill = factor(phenophase_status))) +
+  scale_fill_manual(values = c("grey", "brown")) +
+  ylab('individual') +
+  theme(
+    axis.text.y = element_blank(),
+    plot.background = element_rect(fill = "white"),
+    panel.background = element_rect(fill = "white"),
+    plot.margin = margin(1, 1, 1, 1, unit = "cm"),
+    plot.title = element_text(size = 14),
+    legend.position = "none"
+  ) 
+
+# figure for percentage point
+test <- with_lag %>% 
+  group_by(doy) %>% 
+  summarise(percentage = mean(phenophase_status)) %>% 
+  ggplot() +
+  geom_point(aes(x = doy, y = percentage))

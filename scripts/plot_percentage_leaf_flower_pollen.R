984f4b0f4f2c88656de03f7ba7781c9902e2800a
Liu22Yi <ly2222ny2sz@gmail.com>
2023-07-25 13:52:07 -0400

    finish putting figure about speceis level average lag together

--- scripts/plot_percentage_leaf_flower_pollen.R
+++ scripts/plot_percentage_leaf_flower_pollen.R
@@ -1,46 +1,41 @@
 source("scripts/function_get_lagbufferednpn_percentage.R")
-npn_leaf <- get_buffle_npn_percentage(50,1,"Quercus",10,TRUE)
+npn_leaf <- get_buffle_npn_percentage(50,1,"Quercus",10)
+npn_leaf_lag <- get_buffle_npn_percentage(50,1,"Quercus",10,TRUE)
 npn_flower <- get_buffle_npn_percentage(50,7,"Quercus",10)
 source("scripts/function_get_smoothed_nab_for_taxa.R")
 nab <- get_smoothed_nab("Quercus")
 
 # stardarize the data (should be put into getting data?)
-npn_leaf_n <- npn_leaf %>% 
-  select(station,year, doy,count_w) %>% 
-  rename(leaf = count_w)
-npn_leaf_s <- npn_leaf_n %>% 
-  group_by(station,year) %>% 
-  summarise(total = sum(leaf)) %>% 
-  inner_join(npn_leaf_n, by = c("station","year")) %>% 
-  mutate(leaf = leaf/total) %>% 
-  select(-total) %>% 
-  ungroup()
+standardize_data <- function(data, new_name) {
+  renamed_data <- data %>% 
+    select(station, year, doy, count_w) %>% 
+    rename({{new_name}} := count_w)
+  
+  unified_data <- renamed_data %>% 
+    group_by(station, year) %>% 
+    summarise(total = sum({{new_name}})) %>%
+    inner_join(renamed_data, by = c("station","year")) %>% 
+    mutate({{new_name}} := {{new_name}} / total) %>% 
+    select(-total) %>% 
+    ungroup()
 
-npn_flower_n <- npn_flower %>% 
-  select(station,year, doy,count_w) %>% 
-  rename(flower = count_w)
-npn_flower_s <- npn_flower_n %>% 
-  group_by(station,year) %>% 
-  summarise(total = sum(flower)) %>% 
-  inner_join(npn_flower_n, by = c("station","year")) %>% 
-  mutate(flower = flower/total) %>% 
-  select(-total) %>% 
-  ungroup()
+  
+  return(unified_data)
+}
 
-nab_n <- nab %>% 
-  mutate(doy = lubridate::yday(date)) %>% 
-  select(stationid, year, doy, count_w) %>% 
-  rename(station = stationid, pollen = count_w) 
-nab_s <- nab_n%>% 
-  group_by(station, year) %>% 
-  summarise(total = sum(pollen)) %>%
-  inner_join(nab_n, by = c("station","year")) %>% 
-  mutate(pollen = pollen/total) %>% 
-  select(-total)
+npn_leaf_s <- standardize_data(npn_leaf, leaf)
+npn_leaf_lag_s <- standardize_data(npn_leaf_lag, leaf_lag)
+npn_flower_s <- standardize_data(npn_flower, flower)
+nab_s <- standardize_data(nab %>% 
+                            mutate(doy = lubridate::yday(date))%>% 
+                            rename(station = stationid), pollen)
+
 
 
 # visial check
-to_plot <- full_join(npn_leaf_s,npn_flower_s,by = c("station","year","doy"))  %>% 
+to_plot <- npn_leaf_s %>% 
+  full_join(npn_leaf_lag_s, by = c("station", "year", "doy")) %>% 
+  full_join(npn_flower_s, by = c("station", "year", "doy")) %>% 
   left_join(nab_s,by = c("station","year","doy")) %>% 
   group_by(station,year) %>%
   filter(!all(is.na(pollen)))
@@ -48,36 +43,41 @@ to_plot <- full_join(npn_leaf_s,npn_flower_s,by = c("station","year","doy"))  %>
 stationlist <- unique(to_plot$station)
 
 site_gg <- vector(mode = "list")
-correlation_table <- tibble(year = integer(), leaf_rmse = numeric(), flower_rmse = numeric(), station = character())
 
-rmse <- function(actual, predicted) {
-  sqrt(mean((actual - predicted)^2, na.rm = TRUE))
-}
+correlation_table <- to_plot %>%
+  mutate(pollen = replace_na(pollen, 0)) %>% 
+  group_by(station, year) %>%
+  summarise(cor_leaf = cor(leaf, pollen, method = "spearman"),
+            cor_leaf_lag = cor(leaf_lag, pollen, method = "spearman"),
+            cor_flower = cor(flower, pollen, method = "spearman"))
+
 
 for (i in seq_along(stationlist)) {
+  
+  
   to_plot_station <- to_plot %>% 
     filter(station == stationlist[i]) %>% 
     mutate(pollen = replace_na(pollen, 0)) 
-
-  # Use RMSE in calculation
+  
   cor_data <- to_plot_station %>%
     group_by(year) %>%
-    summarize(leaf_rmse = rmse(leaf, pollen),
-              flower_rmse = rmse(flower, pollen)) %>% 
+    summarize(leaf_correlation = cor(leaf, pollen, method = "spearman",  use = "na.or.complete"),
+              flower_correlation = cor(flower, pollen, method = "spearman",  use = "na.or.complete"),
+              leaf_lag_correlation = cor(leaf_lag, pollen, method = "spearman",  use = "na.or.complete")) %>% 
     mutate(station = stationlist[i])
-  
-  correlation_table <- bind_rows(correlation_table, cor_data)
   
   site_gg[[i]] <-  to_plot_station %>% 
     ggplot() +
     geom_line(aes(x = doy, y = pollen, color = "pollen"), linewidth = 2, alpha = 0.75) +
     geom_line(aes(x = doy, y = leaf, color = "leaf"), linewidth = 1.5, alpha = 0.75) +
     geom_line(aes(x = doy, y = flower, color = "flower"), linewidth = 1, alpha = 0.75) +
+    geom_line(aes(x = doy, y = leaf_lag, color = "leaf_lag"), linewidth = 0.5, alpha = 0.75) +
     facet_wrap(~ year)+
     geom_text(data = cor_data, 
               aes(x = Inf, y = Inf, 
-                  label = paste0("leaf_rmse: ", round(leaf_rmse, 2), "\n", 
-                                 "flower_rmse: ", round(flower_rmse, 2))), 
+                  label = paste0("leaf_corr: ", round(leaf_correlation, 2), "\n",
+                                 "leaf_lag_corr: ", round(leaf_lag_correlation, 2), "\n",
+                                 "flower_corr: ", round(flower_rmse, 2))), 
               hjust = 1, vjust = 1, size = 4) +
     ggtitle(stationlist[i])
 }
--- scripts/plot_quercus_lag_violin.R
+++ scripts/plot_quercus_lag_violin.R
@@ -10,23 +10,12 @@ quercus <- read_rds("/nfs/turbo/seas-zhukai/phenology/NPN/leaf_flower/Quercus.rd
   mutate(lag = flower-leaf)
 
 # get climate data
-raster_tem <- terra::rast("/nfs/turbo/seas-zhukai/climate/CHELSA/climatology/bio1.tif")
 raster_pre <- terra::rast("/nfs/turbo/seas-zhukai/climate/CHELSA/climatology/bio12.tif")
 
 # connect them
 joined_data_name <- quercus %>% 
   left_join(species_code, by = "species_id") %>% 
-  mutate(tem = terra::extract(raster_tem, cbind(longitude, latitude))$bio1,
-         pre = terra::extract(raster_pre, cbind(longitude, latitude))$bio12) 
-
-
-
-chatgpt_output <- joined_data_name %>% 
-  select(tem, pre, common_name, flower, lag, leaf)
-
-write_csv(chatgpt_output,"/nfs/turbo/seas-zhukai/phenology/phenology_leaf_flower_lag/trygpt.csv")
-
-
+  mutate(pre = terra::extract(raster_pre, cbind(longitude, latitude))$bio12) 
 
 #plot rlm
 oak_rlm <- ggplot(joined_data_name,aes(x = leaf, y = flower)) +
@@ -46,99 +35,64 @@ summarised_data <- joined_data_name %>%
             sd_lag = sd(lag, na.rm = TRUE))
 # Draw the plot
 oak_violin <- joined_data_name %>%
-  ggplot(aes(y = lag, x = reorder(common_name, lag, FUN = mean), fill = functional_type)) +
-  geom_violinhalf(position = position_nudge(x = .2, y = 0)) +
+  ggplot(aes(y = lag, x = reorder(common_name, lag, FUN = mean))) +
+  geom_violinhalf(aes(fill = "green"), position = position_nudge(x = .2, y = 0), show.legend = FALSE) +
   geom_jitter(alpha = 0.1, width = 0.15) +
   geom_point(data = summarised_data, aes(y = mean_lag, 
                                          x = reorder(common_name, mean_lag)),shape = 16, size = 4, color = "red", fill = "white") +
   geom_errorbar(data = summarised_data, 
                 aes(ymin = mean_lag - sd_lag, ymax = mean_lag + sd_lag, x = reorder(common_name, mean_lag)), 
                 width = 0.2, color = 'red', inherit.aes = FALSE) +
-  xlab("Species") +
-  ylab("flower day - leafing day") +
+  xlab("") +
+  ylab("Flower day - Leafing day") +
   scale_y_continuous(limits = c(-20, 50)) +
   coord_flip() +
-  labs(fill = "Functional Type")  
+  theme_bw()
+
   
   
- 
-
-##################
-lags_violin <- p + p3 +
-  patchwork::plot_annotation(tag_levels = "A") +
-  patchwork::plot_layout(design = 
-  "A
-  A
-  A
-  A
-  A
-  A
-  A
-  B"
-          )
-
-# plot oak lag-tem
-selected_species <- joined_data_name %>% 
-  filter(common_name %in% c("bur oak", "chestnut oak","nothern red oak","pin oak","white oak"))
-
-
-q1 <- 
-  ggplot(selected_species,aes(x = pre$bio12, y = leaf)) +
-  geom_point(alpha = 0.1) +
-  # geom_smooth(method = "rlm",aes(x = leaf, y = flower), se = FALSE) +
-  xlab("Temperature") +
-  facet_wrap(~ common_name, nrow = 1)
-q2 <- 
-  ggplot(selected_species,aes(x = pre$bio12, y = flower)) +
-  geom_point(alpha = 0.1) +
-  # geom_smooth(method = "rlm",aes(x = leaf, y = flower), se = FALSE) +
-  xlab("Temperature") +
-  facet_wrap(~ common_name, nrow = 1)
-q3 <- 
-  ggplot(selected_species,aes(x = pre$bio12, y = lag)) +
-  geom_point(alpha = 0.1) +
-  # geom_smooth(method = "rlm",aes(x = leaf, y = flower), se = FALSE) +
-  xlab("Temperature") +
-  facet_wrap(~ common_name, nrow = 1)
-
-q1 + q2 + q3 +
-  patchwork::plot_annotation(tag_levels = "A") +
-  patchwork::plot_layout(design = 
-                           "A
-                           B
-                           C"
-  )
-
-q1 <- ggplot(joined_data_name,aes(x = pre$bio12, y = leaf, color = common_name)) +
-  geom_point(alpha = 0.1) +
-  # geom_smooth(method = "rlm",aes(x = leaf, y = flower), se = FALSE) +
-  xlab("Temperature") 
-
-q2 <- ggplot(joined_data_name,aes(x = pre$bio12, y = flower, color = common_name)) +
-  geom_point(alpha = 0.1) +
-  # geom_smooth(method = "rlm",aes(x = leaf, y = flower), se = FALSE) +
-  xlab("Temperature") 
-
-q3 <- ggplot(joined_data_name,aes(x = pre$bio12, y = lag, color = common_name)) +
-  geom_point(alpha = 0.1) +
-  # geom_smooth(method = "rlm",aes(x = leaf, y = flower), se = FALSE) +
-  xlab("Temperature") 
-
-q1+q2+q3+
-  patchwork::plot_annotation(tag_levels = "A") +
-  patchwork::plot_layout(design = 
-                           "ABC"
-  )
-
-joined_data_name %>% 
+# plot leaf_flower precipitation figure 
+flower_leaf <- joined_data_name %>% 
   group_by(common_name) %>% 
-  summarise(avelag = mean(lag),
-            avetem = mean(tem$bio1),
-            avepre = mean(pre$bio12)) %>% 
+  summarise(avepre = mean(pre),
+            aveflower = mean(flower),
+            aveleaf = mean(leaf)) %>%
+  ggplot() +
+  geom_point(aes(x = avepre, y = aveflower, color = "flower"))+
+  geom_point(aes(x = avepre, y = aveleaf, color = "leaf"))+
+  geom_smooth(aes(x = avepre, y = aveflower, color = "flower"), method = "lm", se = FALSE) +
+  geom_smooth(aes(x = avepre, y = aveleaf, color = "leaf"), method = "lm", se = FALSE) +
+  xlab(expression("Average Precipitation (kg m"^-2*" yr"^-1*")")) +  # Corrected x-axis label
+  ylab("Day of Year") +
+  theme_bw() +
+  theme(legend.position = c(.2, .8)) +
+  labs(color = NULL)
+  
+
+# plot lag_pre
+lag_pre <- joined_data_name %>% 
+  group_by(common_name) %>% 
+  summarise(avepre = mean(pre),
+            avelag = mean(lag)) %>%
   ggplot(aes(x = avepre, y = avelag)) +
   geom_point()+
   geom_smooth(method = "lm", se = FALSE) +
-  ggrepel::geom_text_repel(aes(label = common_name), hjust = 0, vjust = 0)
+  ggrepel::geom_text_repel(aes(label = common_name), hjust = 0, vjust = 0) +
+  xlab(expression("Average Precipitation (kg m"^-2*" yr"^-1*")")) +  # Corrected x-axis label
+  ylab("Average Lag Day") +
+  theme_bw()
+
+
+plot <- oak_violin+flower_leaf+lag_pre+
+  patchwork::plot_annotation(tag_levels = "A") +
+  patchwork::plot_layout(design = 
+                           "AB
+                            AC
+                            AC"
+  )
+
+ggsave("/nfs/turbo/seas-zhukai/phenology/phenology_leaf_flower_lag/plot.png", plot, width = 10, height = 8)
+
 
 # report species level lag
 lag_table <- joined_data_name %>% 

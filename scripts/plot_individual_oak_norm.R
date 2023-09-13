cac40a376565dc9407dfe1dbd925095497103b25
Liu22Yi <ly2222ny2sz@gmail.com>
2023-09-13 11:51:26 -0400

    standarize the figure into same scale and add rlm to the figures

--- scripts/plot_individual_oak_norm.R
+++ scripts/plot_individual_oak_norm.R
@@ -8,29 +8,48 @@ standarized_data <- quercus %>%
   ungroup()
 
 
-library(patchwork)
+library(MASS)
 
 
   # Function to create individual plots
-create_plot <- function(data, x_var, y_var) {
-    ggplot(data, aes_string(x = x_var, y = y_var)) +
-      geom_point(alpha = 0.1) +
-      # geom_abline(intercept = 0, slope = 1, color = "red") + # adding a red line with slope 1 and intercept 0
-      labs(
+create_plot <- function(data, x_var, y_var, max_abs_x, max_abs_y) {
+  
+  # slope <- coef(lm(data[[y_var]] ~ data[[x_var]] - 1) )
+  
+  slope <- coef(rlm(data[[y_var]] ~ data[[x_var]] - 1))
+  
+  # Calculate the maximum absolute value in the data for setting y-axis limits
+  
+  ggplot(data, aes(x = data[[x_var]], y = data[[y_var]], color = common_name)) +
+    geom_point(alpha = 0.1) +
+    geom_abline(intercept = 0, slope = slope, color = "red") + # adding a red line with intercept 0
+    geom_text(aes(x = max(data[[x_var]]), y = max(data[[y_var]]), label = paste("Slope:", round(slope, 2))),
+              hjust = 1, vjust = 1, color = "red", size = 4) + # Add the slope label
+    labs(
         x = x_var,
         y = y_var
       ) +
-      theme_minimal()
+      theme_minimal()+ 
+    theme(legend.position="none") + 
+    ylim(-max_abs_y, max_abs_y) + 
+    xlim(-max_abs_x, max_abs_x) # Set y-axis limits centered at 0
 }
   
   
-# Example usage:
-x_vars <- c("winter_avg_temp",  "winter_avg_temp",  "winter_avg_temp","spring_avg_temp","spring_avg_temp", "spring_avg_temp")
-y_vars <- c("lag",  "leaf",  "flower", "lag","leaf","flower")
+x_vars <- c("winter_avg_temp", "spring_avg_temp")
+y_vars <- c("lag", "leaf", "flower")
 
-plots <- lapply(1:length(x_vars), function(i) create_plot(standarized_data, x_vars[i], y_vars[i]))
+combinations <- expand.grid(x=x_vars, y=y_vars)
 
+max_abs_x <- max(abs(standarized_data[, x_vars]))
+max_abs_y <- max(abs(standarized_data[, y_vars]))
 
-plot_individual_varibility <- wrap_plots(plots)
+plots <- lapply(1:nrow(combinations), function(i) {
+  create_plot(standarized_data, combinations$x[i], combinations$y[i], max_abs_x, max_abs_y)
+})
 
+# Combine plots using patchwork
+combined_plot <- wrap_plots(plots, ncol=2)
 
+# Show combined plot with a shared legend
+combined_plot + plot_layout(guides = "collect") & theme(legend.position = "right")

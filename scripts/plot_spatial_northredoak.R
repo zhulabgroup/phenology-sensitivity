1a438a4ac09cb7cd3f1b88a18094e0d55e3bd954
Liu22Yi <ly2222ny2sz@gmail.com>
2023-11-22 14:54:43 -0500

    add error bar to spatial aggregation and slope uncertainty

--- scripts/plot_spatial_northredoak.R
+++ scripts/plot_spatial_northredoak.R
@@ -4,15 +4,55 @@ quercus <- read_rds("/nfs/turbo/seas-zhukai/phenology/phenology_leaf_flower_lag/
 individual_aggre <- quercus %>%
   filter(common_name %in% c("northern red oak")) %>% 
   group_by(individual_id, longitude, latitude, common_name) %>%
-  summarise(across(c(leaf, flower, lag, winter_avg_temp, spring_avg_temp), mean, .names = "{.col}")) %>% 
+  summarise(
+    across(
+      c(leaf, flower, lag, winter_avg_temp, spring_avg_temp),
+      list(
+        Mean = ~mean(.),
+        Min = ~min(.)-mean(.),
+        Max = ~max(.)-mean(.)
+      ),
+      .names = "{.col}_{.fn}"
+    )
+  ) %>%   
   ungroup() %>% 
-  mutate_at(vars(flower, lag, leaf, winter_avg_temp, spring_avg_temp), 
+  mutate_at(vars(flower_Mean, lag_Mean, leaf_Mean, winter_avg_temp_Mean, spring_avg_temp_Mean), 
             ~ . - mean(.)) 
 
 
-source("function_plot_sensitive.R")
+library(plotly)
 
-plots <- plot_sensitive(individual_aggre)
+gg <- ggplot(individual_aggre, aes(x = spring_avg_temp_Mean, y = leaf_Mean, text = spring_avg_temp_Min)) +
+  geom_point() +
+  geom_errorbarh(
+    aes(
+      xmin = spring_avg_temp_Mean + spring_avg_temp_Min,
+      xmax = spring_avg_temp_Mean + spring_avg_temp_Max,
+      y = leaf_Mean
+    ),
+    height = 0.1,  # Adjust as needed
+    alpha = 0.2
+  ) +
+  geom_errorbar(
+    aes(
+      ymin = leaf_Mean + leaf_Min,
+      ymax = leaf_Mean + leaf_Max,
+      x = spring_avg_temp_Mean
+    ),
+    width = 0.1,  # Adjust as needed
+    alpha = 0.2
+  ) +
+  geom_smooth(
+    method = "lm", 
+    formula = y ~ x - 1,  # Linear model with intercept forced to zero
+    se = TRUE,           # Show uncertainty area (confidence interval)
+    color = "blue"       # You can choose the color
+  )+
+  labs(
+    x = "spring_avg_temp",
+    y = "leaf"
+  ) 
+# title = "Scatter Plot with User-Defined Error Bars"
 
-# Show combined plot with a shared legend
-plot_spatial_northernredoak <- wrap_plots(plots, ncol=2) 
+# Convert the ggplot object to a plotly object
+ggplotly(gg)

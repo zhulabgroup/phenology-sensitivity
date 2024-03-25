d9ca5c0fb85e4ccdf1b7f4d19b3dc922222a699a
Liu22Yi <ly2222ny2sz@gmail.com>
2024-03-25 09:22:16 -0400

    organize code

---
+++ scripts/plot_summary_posteriors.R
@@ -0,0 +1,32 @@
+##### summary figure
+posteriors <- readRDS("/Volumes/seas-zhukai/phenology/phenology_leaf_flower_lag/posterior.rds")
+
+to_plot = data.frame()
+for (i in 1:23) {
+  temporal <- posteriors[[i]]$Temporal %>%     
+    as.data.frame() %>%
+    setNames("sample") %>%
+    filter(row_number() %% 10 == 0) %>% 
+    mutate(dim = "T", species = names(posteriors)[i]) 
+  
+  spatial <- posteriors[[i]]$Spatial %>% 
+    as.data.frame() %>%
+    setNames("sample") %>%
+    filter(row_number() %% 10 == 0) %>% 
+    mutate(dim = "S", species = names(posteriors)[i]) 
+  
+  to_plot = rbind(to_plot,temporal,spatial)
+}
+
+library(ggridges)
+
+ggplot(to_plot, aes(x = sample, y = species, height = after_stat(density), group = interaction(species, dim), fill = dim)) +
+  geom_density_ridges(alpha = 0.5, scale = .5) +
+  scale_x_continuous(limits = c(-15, 5)) 
+
+
+to_plot$species <- factor(to_plot$species, levels = rev(unique(to_plot$species)))
+
+ggplot(to_plot, aes(x = sample, y = species, height = after_stat(density), group = interaction(species, dim), fill = dim)) +
+  geom_density_ridges(alpha = 0.5, scale = .5) +
+  scale_x_continuous(limits = c(-15, 5)) 
